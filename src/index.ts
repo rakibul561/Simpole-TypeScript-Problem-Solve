72b47dcfa26a27fe41b20e89af2fb30ec27a4e9d
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-07 11:02:01 -0700

    Problem solve

--- src/index.ts
+++ src/index.ts
@@ -29,10 +29,85 @@ console.log(formatString("Hello", false));  // "hello"
         );
       }
       
-    (filterByRating(books));
-      
+    (filterByRating(books)); 
+
+
      /* ---------------------------------Problem number is 3  ---------------------------------------------------->*/
-     
-     
+
+    function concatenateArrays<T>(...arrays: T[][]): T[] {
+        let newArray: T[] = [];
+        for (let arr of arrays) {
+          newArray.push(...arr);
+        }
+        return newArray;
+      }
       
-  
+      const result = concatenateArrays([1, 2, ], [4, 5, 6]);
+      console.log(result); 
+
+
+
+
+     /* ---------------------------------Problem number is 4  ---------------------------------------------------->*/
+     
+
+
+
+
+
+
+      
+     /* ---------------------------------Problem number is 5  ---------------------------------------------------->*/
+     
+     function processValue(value: string | number):number {
+        if(typeof value === 'number'){
+          return  value * 2 ;
+        } else {
+            return value.length ;
+        } 
+    
+     }
+       
+     processValue("hello"); 
+     processValue(10);         
+
+
+     /* ---------------------------------Problem number is 7  ---------------------------------------------------->*/
+
+
+
+
+
+
+
+
+
+
+
+
+      
+     /* ---------------------------------Problem number is 7  ---------------------------------------------------->*/
+
+
+     enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+      }
+      
+      function getDayType(day: Day): string {
+        if(day === Day.Saturday || day === Day.Sunday){
+            return "Weekend" 
+        }
+         else{
+            return "Weekday"
+         }
+      }
+
+      getDayType(Day.Monday);   // Output: "Weekday"
+      getDayType(Day.Sunday);   // Output: "Weekend"
+    
